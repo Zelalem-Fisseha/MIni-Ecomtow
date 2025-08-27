68b367df9c9aabaeef256c67d22546cd6cc25c74
zelalem-fisseha <zelalem.fisseha@bitscollege.edu.et>
2025-08-27 15:12:54 +0300

    the order done

---
+++ src/main/java/org/miniecom/miniecomtow/Model/Order.java
@@ -0,0 +1,20 @@
+package org.miniecom.miniecomtow.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue
+    private long id;
+}
